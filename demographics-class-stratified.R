e0c44ab38f3560fbda3a759cbb0eac5646af11dd
selenebanuelos <135679586+selenebanuelos@users.noreply.github.com>
2026-08-26 16:39:31 -0700

    Added and modified comments for clarity

--- demographics-class-stratified.R
+++ demographics-class-stratified.R
@@ -1,6 +1,6 @@
 # Author: Selene Banuelos
 # Date: 8/19/2026
-# Description: Create demographics table, stratified by latent class membership
+# Description: Create demographics table, stratified by latent class label
 
 # setup
 library(readstata13)
@@ -48,38 +48,44 @@ analytic_sample <- demo %>%
 
 # manipulate demographics data to match previously generated tables
 demo_modified <- demo %>%
-  mutate(educcat_mom_mod = case_when(educcat_mom == '>=High School Graduate' ~ '>= High School',
-                                     educcat_mom == "Don't know" ~ "Don't know",
-                                     is.na(educcat_mom) ~ NA,
-                                     # all other responses set to < High school
-                                     .default = '< High School'),
-         langcat_pg_mod = case_when(langcat_pg == 'Span Most' ~ 'Mostly Spanish',
-                                    langcat_pg == 'Eng Most' | langcat_pg == 'Both equally' ~ 'Mostly English or Both',
-                                    langcat_pg == 'Other' ~ 'Other',
-                                    is.na(langcat_pg) ~ NA),
-         USyrcat_dl_mod = case_when(USyrcat_dl == 'Entire life' ~ 'U.S. Born',
-                                    is.na(USyrcat_dl) ~ NA,
-                                    # all other responses set to foreign born
-                                    .default = 'Foreign Born'),
-         aces_tot_cat_m_mod = case_when(aces_tot_cat_m == '0' ~ '0',
-                                        aces_tot_cat_m == '1' | aces_tot_cat_m == '2' ~ '1-2',
-                                        is.na(aces_tot_cat_m) ~ NA,
-                                        # all other values set to 3+
-                                        .default = '3+'),
-         ipovcat9y_mod = case_when(ipovcat9y == 'At or below Poverty' ~ 'At or below poverty level',
-                               is.na(ipovcatbl) ~ NA,
-                               # all other values set to above poverty level
-                               .default = 'Above poverty level')
+  mutate(
+    # maternal education
+    educcat_mom_mod = case_when(educcat_mom == '>=High School Graduate' ~ '>= High School',
+                                educcat_mom == "Don't know" ~ "Don't know",
+                                is.na(educcat_mom) ~ NA,
+                                # all other responses set to < High school
+                                .default = '< High School'),
+    # language spoken by mother
+    langcat_pg_mod = case_when(langcat_pg == 'Span Most' ~ 'Mostly Spanish',
+                               langcat_pg == 'Eng Most' | langcat_pg == 'Both equally' ~ 'Mostly English or Both',
+                               langcat_pg == 'Other' ~ 'Other',
+                               is.na(langcat_pg) ~ NA),
+    # years of maternal U.S. residence at delivery
+    USyrcat_dl_mod = case_when(USyrcat_dl == 'Entire life' ~ 'U.S. Born',
+                               is.na(USyrcat_dl) ~ NA,
+                               # all other responses set to foreign born
+                               .default = 'Foreign Born'),
+    # maternal adverse childhood experiences
+    aces_tot_cat_m_mod = case_when(aces_tot_cat_m == '0' ~ '0',
+                                   aces_tot_cat_m == '1' | aces_tot_cat_m == '2' ~ '1-2',
+                                   is.na(aces_tot_cat_m) ~ NA,
+                                   # all other values set to 3+
+                                   .default = '3+'),
+    # household income when child was 9 years old
+    ipovcat9y_mod = case_when(ipovcat9y == 'At or below Poverty' ~ 'At or below poverty level',
+                              is.na(ipovcatbl) ~ NA,
+                              # all other values set to above poverty level
+                              .default = 'Above poverty level')
   )
   
 
-# merge demographics data with assigned class memberships
+# merge demographics data with assigned class labels
 classified <- demo_modified %>%
   # only keep sample used in LCA
   filter(newid %in% analytic_sample) %>%
   # change newid to integer for joining
   mutate(newid = as.integer(newid)) %>%
-  # join demographics data to participant class membership data
+  # join demographics data to participant class label data
   left_join(., 
              classes,
              by = 'newid') %>%
@@ -95,7 +101,7 @@ classified <- demo_modified %>%
   )
   
 # create table -----------------------------------------------------------------
-# characteristic variables to include in table
+# characteristics to include in table
 vars <- c('momdl_age2', 
           'educcat_mom_mod',
           'marcat_pg',
@@ -108,7 +114,7 @@ vars <- c('momdl_age2',
           'ipovcat9y_mod'
 )
 
-# table stratified on asthma trajectories
+# table stratified on asthma trajectories obtained using latent class analysis
 tbl_summary(
   classified,
   include = vars,
@@ -119,6 +125,7 @@ tbl_summary(
   ) %>%
   modify_caption('**Using classes from latent class analysis**')
 
+# table stratified on asthma trajectories obtained using latent class growth models
 tbl_summary(
   classified,
   include = vars,
